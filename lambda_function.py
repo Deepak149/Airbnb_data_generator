7257e27a522db42ae7043e8ba0bf1624550e602c
Deepak Reddy <deepakreddy@Deepaks-MacBook-Pro.local>
2024-03-27 03:10:37 -0700

    increase recrods to 20

--- lambda_function.py
+++ lambda_function.py
@@ -20,7 +20,7 @@ def generate_booking_data():
     
 def lambda_handler(event, context):
     i=0
-    while(i<25):
+    while(i<30):
         sales_order = generate_booking_data()
         print(sales_order)
         sqs_client.send_message(
